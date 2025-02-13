1ddacd6e1eeb92239a59b8a6fae0d34ca1e1e35d
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2025-02-13 16:39:17 +0100

    fix: use string instead of array in fontFamily

--- uno.config.ts
+++ uno.config.ts
@@ -5,16 +5,7 @@ export default defineConfig({
   presets: [presetUno({ dark: "media" })],
   theme: {
     fontFamily: {
-      sans: [
-        "Inter",
-        "ui-sans-serif",
-        "system-ui",
-        "sans-serif",
-        "Apple Color Emoji",
-        "Segoe UI Emoji",
-        "Segoe UI Symbol",
-        "Noto Color Emoji",
-      ],
+      sans: '"Inter", "ui-sans-serif", "system-ui", "sans-serif", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"',
     },
   },
 });
